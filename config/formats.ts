936f0e0bd1cff1e8e0317549f13cd0068ad4ae38
arashivox <147354636+arashivox@users.noreply.github.com>
2023-10-10 16:04:42 +0000

    0

--- config/formats.ts
+++ config/formats.ts
@@ -2321,21 +2321,13 @@ export const Formats: FormatList = [
 		},
 	},
 	{
-		name: "[Gen 6] Mix and Megas Revisited",
+		name: "[Gen 6] AX Doubles",
 		threads: [
 			`&bullet; <a href="https://www.smogon.com/forums/threads/3713949/">Megas Revisited on Smogon Forums</a>`,
 			`&bullet; <a href="https://docs.google.com/spreadsheets/d/1wK11cPHnPCmH7JFss6leKW6_-cumn3DuZA-YMzrzF-U/edit?usp=sharing">Spreadsheet</a>`,
 		],
-		mod: 'gen6mixandmegasrevisited',
-		ruleset: ['Standard', 'Swagger Clause', 'Mega Data Mod'],
-		banlist: [
-			'Medichamite', 'Glalitite', 'Altarianite',
-		],
-		restricted: [
-			'Arceus', 'Cresselia', 'Darkrai', 'Deoxys-Attack', 'Deoxys-Normal', 'Deoxys-Speed', 'Dialga', 'Dragonite', 'Genesect',
-			'Giratina', 'Groudon', 'Ho-Oh', 'Kyogre', 'Kyurem-Black', 'Kyurem-White', 'Lucario', 'Lugia', 'Manaphy', 'Mewtwo',
-			'Palkia', 'Rayquaza', 'Regigigas', 'Reshiram', 'Shaymin-Sky', 'Slaking', 'Xerneas', 'Yveltal', 'Zekrom',		
-		],
+		mod: 'gen6AXdoubles',
+		ruleset: ['standarddoubles', 'Swagger Clause', 'Mega Data Mod'],
 		onValidateTeam(team) {
 			const itemTable = new Set<ID>();
 			for (const set of team) {
