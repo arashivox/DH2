1c23627d6b4319526825a0eec405e4863fe367aa
arashivox <147354636+arashivox@users.noreply.github.com>
2023-10-11 00:52:17 +0000

    removed formats

--- data/mods/roulettemons2/scripts.ts
+++
@@ -1,8 +0,0 @@
-export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
-	inherit: 'gen9',
-	teambuilderConfig: {
-		excludeStandardTiers: true,
-		customTiers: ['R2, R2Ubers'],
-		customDoublesTiers: ['R2, R2Ubers'],
-	},
-};
--- data/mods/vaporemons/learnsets.ts
+++
@@ -1,7 +0,0 @@
-export const Learnsets: {[k: string]: LearnsetData} = {
-	rotomfan: {
-		learnset: {
-			windbreaker: ["9L1"],
-		},
-	},
-};
