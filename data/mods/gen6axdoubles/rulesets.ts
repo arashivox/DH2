4b6c30799ab976cbe22831a2d185e90be8d591ec
arashivox <147354636+arashivox@users.noreply.github.com>
2023-10-13 11:35:48 +0000

    Update rulesets.ts

--- data/mods/gen6axdoubles/rulesets.ts
+++ data/mods/gen6axdoubles/rulesets.ts
@@ -1,4 +1,35 @@
 export const Rulesets: {[k: string]: ModdedFormatData} = {
+	standarddoubles: {
+		effectType: 'ValidatorRule',
+		name: 'Standard Doubles',
+		ruleset: [
+			'Obtainable', 'Team Preview', 'Species Clause', 'Nickname Clause', 'OHKO Clause', 'Sleep Clause Mod', 'Endless Battle Clause', 'HP Percentage Mod', 'Cancel Mod',
+			],
+	},
+	sleepclausemod: {
+		effectType: 'Rule',
+		name: 'Sleep Clause Mod',
+		desc: "Prevents players from putting more than one of their opponent's Pok&eacute;mon to sleep at a time.",
+		onBegin() {
+			this.add('rule', 'Sleep Clause Mod: Limit one foe put to sleep');
+		},
+		onSetStatus(status, target, source) {
+			if (source && source.isAlly(target)) {
+				return;
+			}
+			if (status.id === 'slp') {
+				for (const pokemon of target.side.pokemon) {
+					if (pokemon.hp && pokemon.status === 'slp') {
+						if (!pokemon.statusState.source || !pokemon.statusState.source.isAlly(pokemon)) {
+							this.add('-message', 'Sleep Clause Mod activated.');
+							this.hint("Sleep Clause Mod prevents players from putting more than one of their opponent's Pokémon to sleep at a time");
+							return false;
+						}
+					}
+				}
+			}
+		},
+	},
 	megadatamod: {
 		effectType: 'Rule',
 		name: 'Mega Data Mod',
